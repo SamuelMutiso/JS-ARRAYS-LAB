defdaf29a91eec0a819c6eed8b37eae9d4596327
SamuelMutiso <muuosamuel335@gmail.com>
2026-04-14 23:44:30 +0300

    created an inventory system

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,27 @@
 // Write your code here
+// Crearting thr product inventory array
+// ill now initialize the array with the given required strings in the instructions
+let products = [ "laptop", " phone", "headphones", "monitor"];
 
+// Accessing product information 
+function logFirstProduct () {
+  console.log(products[0]);
+}
+
+// Adding a product
+function addProduct(productName) {
+  products.push(productName);
+}
+
+// updating product information
+function updateProductName (index, newName){
+  products[index] = newName;
+}
+
+// removing a product
+ function removeLastProduct () {
+  products.pop();
+ }
 
 
 // Export the necessary parts for testing
